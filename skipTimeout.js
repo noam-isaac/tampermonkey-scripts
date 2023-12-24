3ab78bf0a0966fbb54ca129c4b298429a16e2405
Noam Isaac <noambugot1@gmail.com>
2023-12-24 15:38:40 +0200

    Update @match URL in skipTimeout.js

--- skipTimeout.js
+++ skipTimeout.js
@@ -4,7 +4,7 @@
 // @version      2023-12-24
 // @description  try to take over the world!
 // @author       You
-// @match        https://timeout.co.il/
+// @match        https://timeout.co.il/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=co.il
 // @grant        none
 // ==/UserScript==
@@ -12,23 +12,19 @@
 (function () {
 	'use strict';
 
+	function clickButton() {
+		const button = document.querySelector('.continue-button');
+		if (button) {
+			button.click();
+			console.log('Clicked button', button);
+		}
+	}
+
+	// Click the button immediately if it exists
+	clickButton();
+
 	// Create a new observer
-	var observer = new MutationObserver(function (mutations) {
-		// For each mutation
-		mutations.forEach(function (mutation) {
-			// If nodes were added
-			if (mutation.addedNodes) {
-				// For each added node
-				[].slice.call(mutation.addedNodes).forEach(function (node) {
-					// If the node has the class 'continue-button'
-					if (node.classList && node.classList.contains('continue-button')) {
-						// Click the button
-						node.click();
-					}
-				});
-			}
-		});
-	});
+	const observer = new MutationObserver(clickButton);
 
 	// Start observing the document with the configured parameters
 	observer.observe(document, { childList: true, subtree: true });

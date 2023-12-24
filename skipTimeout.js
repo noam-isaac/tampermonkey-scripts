59e36ec9ae2bc442ab0a20bb749daf8547d1750e
Noam Isaac <noambugot1@gmail.com>
2023-12-24 15:50:33 +0200

    Refactor clickButtons function and add removeAds function

--- skipTimeout.js
+++ skipTimeout.js
@@ -12,7 +12,7 @@
 (function () {
 	'use strict';
 
-	function clickButtons() {
+	function removeAds() {
 		const buttons = [
 			...document.querySelectorAll('.continue-button'),
 			...document.querySelectorAll('.ad_close'),
@@ -23,13 +23,24 @@
 				console.log('Clicked button', button);
 			}
 		});
+
+		// Find all divs with classes 'sticky_footer-container' and 'show'
+		const divs = document.querySelectorAll('div.sticky_footer-container.show');
+
+		// For each div
+		divs.forEach((div) => {
+			// Remove the 'show' class
+			div.classList.remove('show');
+			// Add the 'hide' class
+			div.classList.add('hide');
+		});
 	}
 
 	// Click the button immediately if it exists
-	clickButtons();
+	removeAds();
 
 	// Create a new observer
-	const observer = new MutationObserver(clickButtons);
+	const observer = new MutationObserver(removeAds);
 
 	// Start observing the document with the configured parameters
 	observer.observe(document, { childList: true, subtree: true });
